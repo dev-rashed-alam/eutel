33636b7c74f7456a28c720444b5721284b052188
dev-rashed-alam <dev.rashedalam@gmail.com>
2025-08-13 21:32:12 +0600

    feat: Added desktop and mobile version ui

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif'],
+                roboto: ['Roboto', 'sans-serif'],
+                sans: ['Roboto', 'sans-serif'], // optional: make Roboto default
+            },
+        },
+    },
+    plugins: [],
+}
